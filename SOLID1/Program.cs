f7fdb313c5c8c25c037dfb5a6d4db0d48171c193
User <User@USER-PC>
2022-05-23 16:21:32 +0400

    Add project files.

---
+++ SOLID1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace SRP
+{
+    public class Employee
+    {
+        public int Employee_Id { get; set; }
+        public string Employee_Name { get; set; }
+
+        /// <summary>
+        /// This method used to insert into employee table
+        /// </summary>
+        /// <param name="em">Employee object</param>
+        /// <returns>Successfully inserted or not</returns>
+        public bool InsertIntoEmployeeTable(Employee em)
+        {
+            // Insert into employee table.
+            return true;
+        }
+        
+    }
+    public class ReportGeneration
+    {
+        /// <summary>
+        /// Method to generate report
+        /// </summary>
+        /// <param name="em"></param>
+        public void GenerateReport(Employee em)
+        {
+            // Report reneration with employee data.
+        }
+    }
+}
